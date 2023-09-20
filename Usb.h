79d1d18f2da910f03a5296ddf38d3a7685e7198a
Bryan Bendall <b_bendall@hotmail.com>
2023-09-20 16:56:58 -0400

    Added Usb Files

---
+++ Usb.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "EBrytecCan.h"
+#include "Usb/UsbDefs.h"
+
+class Usb {
+
+public:
+    Usb() = default;
+
+    static void send(Brytec::CanExtFrame& frame);
+    static void send(Brytec::UsbPacket& packet);
+    static void update();
+};
